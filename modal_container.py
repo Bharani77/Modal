c1f19ab313bf8825a8c02a3a3dd48aeb7e7596cc
Bharani77 <bharani007dharan@gmail.com>
2025-04-17 22:40:36 +0530

    Update modal_container.py

--- modal_container.py
+++ modal_container.py
@@ -11,7 +11,7 @@ app = App(app_name)
 
 # Create a Docker image directly from the Docker Hub image
 image = Image.from_registry(
-    "bharanidharan/galaxykick:v70",
+    "bharanidharan/galaxykick:v71",
     add_python="3.9"
 ).pip_install(
     "requests",
